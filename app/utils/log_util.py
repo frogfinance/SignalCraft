2ff265c38e9e8de32bf99165971d9309a8eb7ca3
Aaron Gooch <eodgooch@gmail.com>
2025-01-23 17:24:20 -0800

    add base logging configuration functionality

---
+++ app/utils/log_util.py
@@ -0,0 +1,38 @@
+def gen_logging_config():
+    """Configure logging with Uvicorn's handlers."""
+    # Base logging configuration
+    logging_config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "()": "uvicorn.logging.DefaultFormatter",
+                "fmt": "%(levelprefix)s %(asctime)s - %(message)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+            },
+            "access": {
+                "()": "uvicorn.logging.AccessFormatter",
+                "fmt": "%(levelprefix)s %(asctime)s - %(client_addr)s - %(request_line)s - %(status_code)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+            },
+        },
+        "handlers": {
+            "default": {
+                "formatter": "default",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
+            "access": {
+                "formatter": "access",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "loggers": {
+            "uvicorn": {"handlers": ["default"], "level": "INFO", "propagate": False},
+            "uvicorn.error": {"handlers": ["default"], "level": "INFO", "propagate": False},
+            "uvicorn.access": {"handlers": ["access"], "level": "INFO", "propagate": False},
+            "app": {"handlers": ["default"], "level": "INFO", "propagate": False},
+        },
+    }
+    return logging_config
