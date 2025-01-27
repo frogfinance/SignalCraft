889fdc1d8390aa25fb2c74ce404ee8b5707b3e7d
Aaron Gooch <eodgooch@gmail.com>
2025-01-26 21:29:33 -0800

    fix fetch data bugs and query each ticker individually

--- app/handlers/data_handler.py
+++ app/handlers/data_handler.py
@@ -17,34 +17,63 @@ class DataHandler():
         self.timeframe = timeframe
 
     def fetch_data(self, start=None, end=None, days=1, use_most_recent=False):
+        """
+        Fetch candle data for the specified tickers and timeframe.
+        start is the start date for the data fetch. If None, it defaults to the current date minus `days`.
+        end is the end date for the data fetch. If None, it defaults to the current date.
+        days is the number of days to fetch data for. If None, it defaults to 1. start and end if specified will override this.
+        use_most_recent is a flag to set start as the most recent candle data timestamp.
+        Iterates by the number of days specified in the `days` parameter.
+        """
         end = datetime.now() if end is None else end
         start = end - timedelta(days=days) if start is None else start
-
-        get_data_for_tickers = self.tickers
         
-        try:
-            if use_most_recent:
-                # find the most recent candle timestart as `start``
-                connection = duckdb.connect(f"{self.db_base_path}/{get_data_for_tickers[0]}_{self.timeframe}_data.db")
+        # set start value to the most recent candle timestamp
+        if use_most_recent:
+            # find the most recent candle timestart as `start``
+            oldest_candle = None
+            for ticker in self.tickers:
+                
+                connection = duckdb.connect(f"{self.db_base_path}/{ticker}_{self.timeframe}_data.db")
                 # get most recent candle from the db
                 most_recent_candle_data = connection.sql(
                     f"SELECT * FROM ticker_data ORDER BY timestamp DESC LIMIT 1"
-                )
+                ).df()
                 connection.close()
-                start = most_recent_candle_data["timestamp"].iloc[0] if not most_recent_candle_data.empty else start
-            
-            request = StockBarsRequest(
-                symbol_or_symbols=get_data_for_tickers,
-                start=start,
-                end=end,
-                timeframe=self.timeframe,
-            )
-            data = self.data_store.get_stock_bars(request)
-            if data.data is None or get_data_for_tickers[0] not in data.data.keys():
-                logger.info("No data received", data)
-                return None
-            else:    
-                return data.data
+                last_candle = most_recent_candle_data["timestamp"].iloc[0] if not most_recent_candle_data.empty else None
+                if oldest_candle and last_candle and last_candle < oldest_candle:
+                    oldest_candle = last_candle
+                elif not oldest_candle:
+                    oldest_candle = last_candle
+            start = oldest_candle if oldest_candle else start
+        
+
+        try:
+            curr_start = start
+            curr_end = start + timedelta(days=1)
+            while curr_start <= end:
+                for ticker in self.tickers:
+                    logger.info(f"Fetching data for {ticker} from {curr_start} to {curr_end}")
+                    data = None
+                    request = StockBarsRequest(
+                        symbol_or_symbols=[ticker],
+                        start=curr_start,
+                        end=curr_end,
+                        timeframe=self.timeframe,
+                    )
+                    try:
+                        data = self.data_store.get_stock_bars(request)
+                    except Exception as e:
+                        logger.error(f"Error fetching market data for ticker:{ticker} error; {e}")
+                    
+                    if data and data.data is None:
+                        logger.info("No data received", data)
+                    else:    
+                        logger.info(f"Data received for {ticker} from {curr_start} to {curr_end}")
+                        self.save_market_data(data.data)
+                    logger.info(f"Data saved for {ticker}")
+                curr_start = curr_end
+                curr_end = curr_start + timedelta(days=1)
         except Exception as e:
             logger.error(f"Error fetching market data: {e}")
             return None
@@ -62,13 +91,14 @@ class DataHandler():
         for ticker in data.keys():
             ticker_data = data.get(ticker, [])
             for row in ticker_data:
-                row_str = f"('{row.timestamp}', '{ticker}', {row.open}, {row.high}, {row.low}, {row.close}, {row.volume}, {row.vwap})"
-                self.save_to_db(ticker, row_str, db_base_path=self.db_base_path, timeframe=self.timeframe)
+                value_str = f"('{row.timestamp}', '{ticker}', {row.open}, {row.high}, {row.low}, {row.close}, {row.volume}, {row.vwap})"
+                logger.info(f"candle values for {ticker}: {value_str}")
+                self.save_to_db(ticker, value_str)
             
-            logger.info('Data saved for ticker', ticker)
+            logger.info('Data saved for ticker {}'.format(ticker))
 
-    def save_to_db(self, ticker, data):
+    def save_to_db(self, ticker, value_str):
         db_path = f"{self.db_base_path}/{ticker}_{self.timeframe}_data.db"
         conn = duckdb.connect(db_path)
-        conn.execute(f"INSERT OR IGNORE INTO ticker_data VALUES {data}")
+        conn.execute(f"INSERT OR IGNORE INTO ticker_data VALUES {value_str}")
         conn.close()
